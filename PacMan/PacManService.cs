24c569b14e4d8583025789cd1a56ec9357f6ee1d
adsieg01 <adsieg01@gmail.com>
2018-01-21 10:17:30 -0500

    Initial Commit

---
+++ PacMan/PacManService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace PacMan
+{
+    class PacManService
+    {
+        public void ReadGameFile()
+        {
+            string text = @"C:\Users\Andrew\source\repos\PacMan";
+            string[] levelOne = File.ReadAllText("text").Split(',');
+            
+        }
+
+        
+    }
+}
